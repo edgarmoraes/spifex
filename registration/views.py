d26952b9167293e31eba5f79f534ff8ced5f5d4b
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2024-03-15 01:53:37 -0300

    normal testing

--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -10,17 +10,29 @@ def HomePage(request):
 
 def SignupPage(request):
     if request.method=='POST':
-        uname=request.POST.get('username')
+        username=request.POST.get('username')
         email=request.POST.get('email')
         password1=request.POST.get('password1')
         password2=request.POST.get('password2')
 
         if password1 != password2:
             messages.error(request, "Your passwords do not match.")
-            return render(request, 'signup.html', {'username': uname, 'email': email})
-        else:
+            return render(request, 'signup.html', {'username': username, 'email': email})
 
-            my_user=User.objects.create_user(uname,email,password1)
+        username_exists = User.objects.filter(username=username).exists()
+        email_exists = User.objects.filter(email=email).exists()
+
+        if username_exists and email_exists:
+            messages.error(request, "Username and email are already in use.")
+            return render(request, 'signup.html')
+        elif username_exists:
+            messages.error(request, "Username is already in use.")
+            return render(request, 'signup.html', {'email': email})
+        elif email_exists:
+            messages.error(request, "Email is already in use.")
+            return render(request, 'signup.html', {'username': username})
+        else:
+            my_user = User.objects.create_user(username, email, password1)
             my_user.save()
             return redirect('login')
 
