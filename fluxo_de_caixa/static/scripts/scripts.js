e54acc58f38647d23a568b3f01b42e6945711cf9
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2024-04-08 23:43:19 -0300

    new tabs and css ajustments

--- fluxo_de_caixa/static/scripts/scripts.js
+++ fluxo_de_caixa/static/scripts/scripts.js
@@ -1,3 +1,22 @@
+function changeTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// To display the first tab by default
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector('.tab-link').click();
+});
+
 // Evitar bancos iguais e datas futuras no processo de transferência
 document.addEventListener("DOMContentLoaded", function() {
   const formularioTransferencia = document.querySelector(".modal-form-transferencias");
