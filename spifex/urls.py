8c594c0319ab9c1e4c9ffcc0e186c7f9291399a7
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2024-04-14 02:28:15 -0300

    updates in settled entries table

--- spifex/urls.py
+++ spifex/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('accounts/', include('registration.urls')),
     path('home/', views.HomePage, name='home'),
     path('logout/', views.LogoutPage, name='logout'),
-    path('plano_de_contas/', include(('chart_of_accounts.urls', 'chart_of_accounts'), namespace='chart_of_accounts')),
+    path('configuracoes/plano_de_contas/', include(('chart_of_accounts.urls', 'chart_of_accounts'), namespace='chart_of_accounts')),
 ]
