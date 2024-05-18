7cc575e05fa6da3307c2276c2dae8f6fd8bb15f8
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2024-05-17 21:41:13 -0300

    ajustment for deleting banks

--- settings/urls.py
+++ settings/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('departamentos/', views.departments, name='departments'),
     path('salvar_banco/', views.save_bank, name='save_bank'),
     path('salvar_departamento/', views.save_department, name='save_department'),
-    path('verificar_e_excluir_banco/<int:idBanco>/', views.verify_and_delete_bank, name='verify_and_delete_bank'),
+    path('verificar_e_excluir_banco/<int:bank_id>/', views.verify_and_delete_bank, name='verify_and_delete_bank'),
     path('verificar_e_excluir_departamento/<int:idDepartamento>/', views.verify_and_delete_department, name='verify_and_delete_department'),
 ]
