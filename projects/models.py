bc585723e2a43e622e93b93a4cbedc19d76e0614
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2024-05-23 20:16:12 -0300

    save_project

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    project_name = models.CharField(max_length = 100)
+    project_code = models.CharField(max_length = 100)
+    project_type = models.CharField(max_length = 100)
+    project_description = models.CharField(max_length = 256)
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from .models import Project
 
 def projects(request):
     return render(request, 'projects.html')
 
 def save_project(request):
-    return render(request, 'projects.html')
+    if request.method == 'POST':
+        project_name = request.POST.get('project_name')
+        project_code = request.POST.get('project_id')
+        project_type = request.POST.get('project_type')
+        project_description = request.POST.get('project_description')
+
+        project = Project(
+            project_name=project_name,
+            project_code=project_code,
+            project_type=project_type,
+            project_description=project_description
+        )
+        project.save()
+
+        return JsonResponse({'success': True})
+    return JsonResponse({'success': False})
